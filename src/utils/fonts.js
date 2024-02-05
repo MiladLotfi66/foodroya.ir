b4fd1778e2ab56ef22f03bfbca830cd7167fcbf1
miladlotfi <miladlotfi66@gmail.com>
2024-02-05 18:37:30 +0330

    add font to layout and tailwind fonts 2

---
+++ src/utils/fonts.js
@@ -0,0 +1,23 @@
+import localFont from "next/font/local";
+
+export const dana = localFont({
+  src: [
+    {
+      path: "../../public/fonts/Dana/woff2/DanaFaNum-Regular.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Dana/woff2/DanaFaNum-Medium.woff2",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Dana/woff2/DanaFaNum-DemiBold.woff2",
+      weight: "400",
+      style: "normal",
+    },
+  
+  
+  ],
+});
