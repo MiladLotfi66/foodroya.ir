60edf2b2fb1852704ebf8ebe99c85e249b033d04
miladlotfi <miladlotfi66@gmail.com>
2024-03-13 10:27:54 +0330

    complete  signup autorize middelware  api

--- src/middleware.js
+++ src/middleware.js
@@ -1,16 +1,16 @@
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
-export async function middleware(req, res) {
+export async function middleware(req) {
   const token = await getToken({ req, secret: process.env.JWT_SECRET });
   const { pathname } = req.nextUrl;
 
-  if (pathname.includes("/signup")||pathname.includes("/signin")|| token) {
+  if ((pathname.includes("/signup") || pathname.includes("/signin")) && token) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+  if (!token && (pathname !== "/signup" || pathname !== "/signin")) {
     return NextResponse.next();
   }
-  if (!token && pathname !== "/signin") {
-        return NextResponse.redirect(new URL("/signin", req.url));  }
-  }
- 
-export { default } from "next-auth/middleware"
-export const config = { matcher: ["/signup", "/signin"] }
+}
 
+export { default } from "next-auth/middleware";
+export const config = { matcher: ["/signup", "/signin"] };
