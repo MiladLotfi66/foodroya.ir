a436d19ab5d79f5913d41779257c799680bb9d51
milad lotfi <miladlotfi66@gmail.com>
2024-04-27 13:32:10 -0400

    add pwa configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,25 @@
-/** @type {import('next').NextConfig} */
+
+import withPWAInit from "@ducanh2912/next-pwa";
+
 const nextConfig = {
   
-    images: {
-        dangerouslyAllowSVG: true,
-        contentDispositionType: 'attachment',
-        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-      },
-    
+  images: {
+      dangerouslyAllowSVG: true,
+      contentDispositionType: 'attachment',
+      contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    },
+  
 };
 
 
-export default nextConfig;
-
+export default withPWAInit({
+  dest: "public",
+  cacheOnFrontendNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+})(nextConfig);
