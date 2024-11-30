4e268b27100ad0c799dc9e6b89e1f0225a345a5a
milad <miladlotfi66@gmail.com>
2024-11-30 18:08:22 +0330

    pagination product ok

--- src/components/templates/panel/Product/Pagination.js
+++ src/components/templates/panel/Product/Pagination.js
@@ -1,6 +1,6 @@
 // src/components/Pagination.js
 
-import React from 'react';
+import React from "react";
 
 const Pagination = ({ currentPage, totalPages, onPageChange }) => {
   if (totalPages === 0) return null;
@@ -16,31 +16,32 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
     <div className="flex justify-center my-4">
       <nav className="inline-flex -space-x-px">
         <button
-          onClick={() => onPageChange(currentPage - 1)}
-          disabled={currentPage === 1}
-          className="px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 disabled:opacity-50"
-        >
-          قبلی
-        </button>
-        {pages.map(page => (
-          <button
-            key={page}
-            onClick={() => onPageChange(page)}
-            className={`px-3 py-2 leading-tight border border-gray-300 ${
-              page === currentPage
-                ? 'text-blue-600 bg-blue-50 hover:bg-blue-100 hover:text-blue-700'
-                : 'text-gray-500 bg-white hover:bg-gray-100 hover:text-gray-700'
-            }`}
-          >
-            {page}
-          </button>
-        ))}
-        <button
           onClick={() => onPageChange(currentPage + 1)}
           disabled={currentPage === totalPages}
           className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 disabled:opacity-50"
         >
           بعدی
+        </button>
+        {pages.map((page) => (
+          <button
+            key={page}
+            onClick={() => onPageChange(page)}
+            className={`px-3 py-2 leading-tight border border-gray-300 ${
+              page === currentPage
+                ? "text-blue-600 bg-blue-50 hover:bg-blue-100 hover:text-blue-700"
+                : "text-gray-500 bg-white hover:bg-gray-100 hover:text-gray-700"
+            }`}
+          >
+            {page}
+          </button>
+        ))}
+
+        <button
+          onClick={() => onPageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+          className="px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 disabled:opacity-50"
+        >
+          قبلی
         </button>
       </nav>
     </div>
